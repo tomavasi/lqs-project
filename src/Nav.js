62dc309f2685f47e744cbfec126352cada2af463
tomavasi <tomaras.vasi@outlook.com>
2023-03-08 18:09:46 +0100

    New files

---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+export default function Nav () {
+    return (
+    <nav>
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Explore</li>
+      </ul>
+    </div>
+    </nav>)}
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+function Navbar() {
+  return (
+    <div>
+     <nav className='navbar'>
+        <div className="navbar-container">
+            <Link to="/" className="navbar-log">
+
+            </Link>
+        </div>
+     </nav>
+    </div>
+  )
+}
+
+export default Navbar
