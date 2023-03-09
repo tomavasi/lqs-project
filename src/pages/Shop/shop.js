0063bc5fc7a0677941bc4ca27190f48a9ea094a0
tomavasi <tomaras.vasi@outlook.com>
2023-03-09 22:48:15 +0100

    new changes in the state

--- src/pages/Shop/shop.js
+++ src/pages/Shop/shop.js
@@ -5,20 +5,20 @@ import { Product } from './product'
 
 export const Shop = () => {
 
-   const [products, setProducts] = useState();
+   const [products, setProducts] = useState([]);
 
     useEffect(
 
         function apiData () {
-            axios.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")
+            axios
+            .get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")
             .then(response =>{
                 setProducts(response.data);
                 });
             
-            }, []);
+            },[]);
 
-   
-
+  
 
   return (
     <div className="shop">
@@ -26,7 +26,7 @@ export const Shop = () => {
         <h1> Little Queer Shop</h1>
         </div> 
         <div className='products'>
-          { products.map((product) =>( <Product data={product} />))}
+          { products.map(product =>( <Product data={product} />)) }
         </div>
     </div>
   )
